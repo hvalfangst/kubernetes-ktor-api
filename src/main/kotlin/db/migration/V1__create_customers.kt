dd11e7c6cd82c87546566e6bfb21c6d9a3dfdbe4
hvalfangst <hvalfangst@tidehunter.eu>
2024-02-24 22:20:29 +0100

    Removed test data from migration scripts as this will only interfere with validation

--- src/main/kotlin/db/migration/V1__create_customers.kt
+++ src/main/kotlin/db/migration/V1__create_customers.kt
@@ -1,65 +1,18 @@
 package db.migration
 
+import model.Customers
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
-import model.*
 
 class V1__create_customers: BaseJavaMigration() {
     override fun migrate(context: Context?) {
         transaction {
             createCustomersTable()
-            insertCustomer("Luke Skywalker", "Tatooine", "luke@skywalker.com", "19 BBY")
-            insertCustomer("Leia Organa", "Alderaan", "leia@organa.com", "19 BBY")
-            insertCustomer("Han Solo", "Corellia", "han@solo.com", "29 BBY")
-            insertCustomer("Chewbacca", "Kashyyyk", "chewie@wookiee.com", "200 BBY")
-            insertCustomer("Obi-Wan Kenobi", "Stewjon", "obi-wan@kenobi.com", "57 BBY")
-            insertCustomer("Yoda", "Unknown", "yoda@jediorder.com", "896 BBY")
-            insertCustomer("Lando Calrissian", "Socorro", "lando@calrissian.com", "31 BBY")
-            insertCustomer("Mace Windu", "Haruun Kal", "mace@windu.com", "72 BBY")
-            insertCustomer("Qui-Gon Jinn", "Unknown", "qui-gon@jinn.com", "92 BBY")
-            insertCustomer("Padmé Amidala", "Naboo", "padme@amidala.com", "46 BBY")
-            insertCustomer("Jar Jar Binks", "Naboo", "jar-jar@binks.com", "52 BBY")
-            insertCustomer("C-3PO", "Unknown", "c-3po@droid.com", "112 BBY")
-            insertCustomer("R2-D2", "Naboo", "r2-d2@droid.com", "33 BBY")
-            insertCustomer("Darth Vader", "Executor-class Star Dreadnought", "darth.vader@sith.empire", "41.9 BBY")
-            insertCustomer("Emperor Palpatine", "Imperial Palace", "palpatine@sith.empire", "84 BBY")
-            insertCustomer("Kylo Ren", "Supremacy-class Star Destroyer", "kylo.ren@firstorder.com", "5 ABY")
-            insertCustomer("Count Dooku", "Geonosis", "dooku@sith.empire", "102 BBY")
-            insertCustomer("Asajj Ventress", "Rattatak", "ventress@sith.empire", "42 BBY")
-            insertCustomer("Grand Moff Tarkin", "Death Star I", "tarkin@empire.com", "64 BBY")
-            insertCustomer("Director Krennic", "Imperial Security Complex", "krennic@empire.com", "51 BBY")
-            insertCustomer("Boba Fett", "Slave I", "boba.fett@bountyhunter.com", "31 BBY")
-            insertCustomer("Jabba the Hutt", "Tatooine", "jabba@huttcartel.com", "600 BBY")
-            insertCustomer("Greedo", "Mos Espa", "greedo@bountyhunter.com", "44 BBY")
-            insertCustomer("IG-88", "Assassin Droid Factory", "ig-88@droid.net", "15 BBY")
-            insertCustomer("Cad Bane", "Malastare", "cad.bane@bountyhunter.com", "67 BBY")
-            insertCustomer("General Grievous", "Invisible Hand", "grievous@sith.empire", "Unknown")
-            insertCustomer("Jango Fett", "Kamino", "jango.fett@bountyhunter.com", "66 BBY")
-            insertCustomer("Ahsoka Tano", "Shili", "ahsoka.tano@jediorder.com", "36 BBY")
-            insertCustomer("Anakin Skywalker", "Tatooine", "anakin.skywalker@jediorder.com", "41.9 BBY")
-            insertCustomer("Jyn Erso", "Vallt", "jyn.erso@rebelalliance.com", "22 BBY")
-            insertCustomer("Cassian Andor", "Fest", "cassian.andor@rebelalliance.com", "26 BBY")
-            insertCustomer("Baze Malbus", "Jedha", "baze.malbus@rebelalliance.com", "55 BBY")
-            insertCustomer("Chirrut Îmwe", "Jedha", "chirrut.imwe@guardianofwhills.com", "52 BBY")
-            insertCustomer("K-2SO", "Unknown", "k2so@droid.com", "Unknown")
-            insertCustomer("Director Orson Krennic", "Imperial Security Complex", "orson.krennic@empire.com", "52 BBY")
-            insertCustomer("Enfys Nest", "Unknown", "enfys.nest@cloudridergang.com", "Unknown")
-            insertCustomer("Dryden Vos", "Corellia", "dryden.vos@crimelord.com", "52 BBY")
         }
     }
     private fun createCustomersTable() {
         SchemaUtils.create(Customers)
-    }
-
-    private fun insertCustomer(name: String, address: String, email: String, dateOfBirth: String) {
-        Customers.insert {
-            it[this.name] = name
-            it[this.address] = address
-            it[this.email] = email
-            it[this.dateOfBirth] = dateOfBirth
-        }
     }
 }
--- src/main/kotlin/db/migration/V3__create_loans.kt
+++ src/main/kotlin/db/migration/V3__create_loans.kt
@@ -1,35 +1,19 @@
 package db.migration
 
-import model.Accounts
 import model.Loans
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.math.BigDecimal
 
 class V3__create_loans: BaseJavaMigration() {
     override fun migrate(context: Context?) {
         transaction {
             createLoansTable()
-            insertLoan(1, BigDecimal(5000.0), BigDecimal(0.05), 30)
-            insertLoan(2, BigDecimal(834000.0), BigDecimal(3.5), 15)
-            insertLoan(2, BigDecimal(2340.0), BigDecimal(10.5), 5)
-            insertLoan(2, BigDecimal(5000.0), BigDecimal(15.5), 2)
         }
     }
 
     private fun createLoansTable() {
         SchemaUtils.create(Loans)
-    }
-
-    private fun insertLoan(customerId: Int, loanAmount: BigDecimal, interestRate: BigDecimal, duration: Int) {
-        Loans.insert {
-            it[this.customerId] = customerId
-            it[this.loanAmount] = loanAmount
-            it[this.interestRate] = interestRate
-            it[this.duration] = duration
-        }
     }
 }
