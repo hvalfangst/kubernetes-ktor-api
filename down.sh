35b0701a9136a6a5bd3dfaeb0f4f3f93a34da46d
hvalfangst <hvalfangst@tidehunter.eu>
2024-02-25 22:44:20 +0100

    Implemented the option to run locally with gradle where resources are provisioned by docker-compose and environment variables explicitly set. Startup and shutdown scripts have been updated accordingly.

--- down.sh
+++ down.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-echo
+# Check if the first argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <local|k8s>"
+  exit 1
+fi
 
-kubectl delete -f k8s/secrets.yml > /dev/null 2>&1
-kubectl delete -f k8s/database.yml > /dev/null 2>&1
-kubectl delete -f k8s/nginx-config.yml > /dev/null 2>&1
-kubectl delete -f k8s/nginx.yml > /dev/null 2>&1
-kubectl delete -f k8s/api.yml  > /dev/null 2>&1
+if [ "$1" = "local" ]; then
+  docker-compose -f local/docker-compose.yml down
+  exit $?
+fi
 
-echo "Deleting resources defined in .yaml files"
-./misc/progress_bar.sh 0.10
+if [ "$1" = "kubernetes" ]; then
+  kubectl delete -f k8s/secrets.yml > /dev/null 2>&1
+  kubectl delete -f k8s/database.yml > /dev/null 2>&1
+  kubectl delete -f k8s/nginx-config.yml > /dev/null 2>&1
+  kubectl delete -f k8s/nginx.yml > /dev/null 2>&1
+  kubectl delete -f k8s/api.yml  > /dev/null 2>&1
+
+  echo "Deleting resources defined in .yaml files"
+  ./misc/progress_bar.sh 0.10
+fi
+
+echo "Invalid argument. Use: $0 <local|k8s>"
+exit 1
--- up.sh
+++ up.sh
@@ -1,60 +1,70 @@
 #!/bin/sh
 
-#  echo "Build image [hvalbanken-api] from Dockerfile"
-#  if ! docker build -t hvalbanken-api:666 .; then
-#      echo
-#      echo "[Error building image 'hvalbanken-api' - Exiting script]"
-#      exit 1
-#  fi
+DB_URL="5432/postgres?user=postgres&password=cookiecutter"
 
-echo -e "\n\n"
+# Check if the first argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <local|k8s>"
+  exit 1
+fi
 
+if [ "$1" = "local" ]; then
+  echo "Running locally..."
 
-kubectl apply -f k8s/database.yml > /dev/null 2>&1
-echo "Creating DB..."
-./misc/progress_bar.sh 0.10
+  docker-compose -f local/docker-compose.yml up -d
+  export DATABASE_URL="localhost:$DB_URL"
+  export POD_NAME="LOCAL"
+  ./gradlew build
+  ./gradlew run
+  exit $?
+fi
 
+if [ "$1" = "kubernetes" ]; then
+  echo "Deploying to Kubernetes..."
 
-# Prompt user for desired encryption key, which is used for crypto operations on tokens
-read -p "Enter desired encryption key: " encryption_key
+  echo "Build image [hvalbanken-api] from Dockerfile"
+    if ! docker build -t hvalbanken-api:666 .; then
+        echo
+        echo "[Error building image 'hvalbanken-api' - Exiting script]"
+        exit 1
+    fi
 
-echo -e "\n\n"
+  echo -e "\n\n"
 
-# Base64 encode the provided encryption key
-encoded_encryption_key=$(echo -n "$encryption_key" | base64 | tr -d '\n')
+  kubectl apply -f k8s/database.yml > /dev/null 2>&1
+  echo "Creating DB..."
+  ./misc/progress_bar.sh 0.10
 
-# Set field encryption-key in secrets.yml to point to the recently encoded encryption key
-sed -i "s|^\(.*encryption-key: \)\(.*\)|\1$encoded_encryption_key|" k8s/secrets.yml
+  # Base64 encode URL associated with our Postgres container
+  b64_db_url=$(echo -n "db:$DB_URL" | base64 | tr -d '\n')
 
-# Create database URL based on service name associated with Postgres deployment
-db_url="db:5432/postgres?user=postgres&password=admin"
+  # Use 'sed' to overwrite value of the field "database-url" contained in yaml file "secrets"
+  sed -i "s|^\(.*database-url: \)\(.*\)|\1$b64_db_url|" k8s/secrets.yml
 
-# Base64 encode URLs
-b64_db_url=$(echo -n "$db_url" | base64 | tr -d '\n')
+  # Create a k8s secret based on contents of manifest file "secrets.yml"
+  kubectl apply -f k8s/secrets.yml > /dev/null 2>&1
 
-# Use 'sed' to overwrite value of the field "database-url" contained in yaml file "secrets"
-sed -i "s|^\(.*database-url: \)\(.*\)|\1$b64_db_url|" k8s/secrets.yml
+  echo "Creating secrets..."
+  ./misc/progress_bar.sh 0.10
 
-# Create a k8s secret based on contents of manifest file "secrets.yml"
-kubectl apply -f k8s/secrets.yml > /dev/null 2>&1
+  kubectl apply -f k8s/nginx-config.yml > /dev/null 2>&1
+  echo "Creating Nginx config..."
+  ./misc/progress_bar.sh 0.10
 
-echo "Creating secrets..."
-./misc/progress_bar.sh 0.10
+  kubectl apply -f k8s/nginx.yml > /dev/null 2>&1
+  echo "Creating Nginx..."
+  ./misc/progress_bar.sh 0.10
 
-kubectl apply -f k8s/nginx-config.yml > /dev/null 2>&1
-echo "Creating Nginx config..."
-./misc/progress_bar.sh 0.10
+  kubectl apply -f k8s/api.yml > /dev/null 2>&1
+  echo "Creating API..."
+  ./misc/progress_bar.sh 0.10
 
-kubectl apply -f k8s/nginx.yml > /dev/null 2>&1
-echo "Creating Nginx..."
-./misc/progress_bar.sh 0.10
+  echo "Preparing pods..."
+  ./misc/progress_bar.sh 0.10
 
-kubectl apply -f k8s/api.yml > /dev/null 2>&1
-echo "Creating API..."
-./misc/progress_bar.sh 0.10
+  # List pods
+  kubectl get pods
+fi
 
-echo "Preparing pods..."
-./misc/progress_bar.sh 0.10
-
-# List pods
-kubectl get pods
+echo "Invalid argument. Use: $0 <local|k8s>"
+exit 1
